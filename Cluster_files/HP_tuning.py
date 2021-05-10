46c2fc50cb6af84906c2d83eaef5f8f6bcd93c93
RaphaelPellegrin <raphael.pellegrin74@gmail.com>
2021-05-10 01:44:56 +0100

    Addingclusterfiles

---
+++ Cluster_files/HP_tuning.py
@@ -0,0 +1,59 @@
+from loading_imagenet import load_data
+import os
+import tensorflow_datasets as tfds
+import keras
+from keras import backend as K
+from keras.metrics import categorical_crossentropy
+from keras.models import Model
+from keras.applications import MobileNet
+import numpy as np
+from keras.optimizers import Adam
+import tensorflow as tf
+import time
+import wandb
+from wandb.keras import WandbCallback
+from keras.callbacks import ModelCheckpoint, LambdaCallback
+import tempfile
+
+global spark
+spark = False
+
+
+def add_regularization(model, penalty):
+	regularizer=tf.keras.regularizers.l2(penalty)
+	for layer in model.layers:
+        	for attr in ['kernel_regularizer']:
+        		if hasattr(layer, attr):
+        			setattr(layer, attr, regularizer)
+	model_json = model.to_json()
+	tmp_weights_path = os.path.join(tempfile.gettempdir(), 'tmp_weights.h5')
+	model.save_weights(tmp_weights_path)
+	model = tf.keras.models.model_from_json(model_json)
+	model.load_weights(tmp_weights_path, by_name=True)
+	return model	
+
+
+def train_model():
+	sweep_config = dict(lr=1e-3, batch_size=64, regularization=5E-4)
+	wandb.init(config=sweep_config)
+    	config = wandb.config
+	batch_size = config.batch_size
+	ds_train, ds_val = load_data(batch_size, spark)   # at this point, we modified load_data in order to output only data with 200 classes
+	initial_learning_rate = config.lr
+	epochs = 25
+	devices = ['GPU:0', 'GPU:1', 'GPU:2', 'GPU:3']
+	strategy = tf.distribute.MirroredStrategy(devices=devices)
+	with strategy.scope():
+		model_MNV2 = tf.keras.applications.MobileNetV2(input_shape=(112, 112, 3), alpha=1.0, include_top=True, weights=None, classes=1000, classifier_activation='softmax')
+		lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(initial_learning_rate, decay_steps=2500, decay_rate=0.98, staircase=True) 
+		m = tf.keras.metrics.TopKCategoricalAccuracy(k=5)
+		optimizer = tf.keras.optimizers.Adam()
+		model_MNV2 = add_regularization(model_MNV2, penalty=config.regularization)
+		model_MNV2.compile(optimizer, loss='categorical_crossentropy', metrics=['accuracy', m])
+	history = model_MNV2.fit(ds_train, validation_data=ds_val, epochs=epochs, verbose = 1)
+	return history
+
+
+
+if __name__=='__main__':
+	train_model()
